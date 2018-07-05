4c11217aca704922aa9b62ced05e27cad6c4819e
Andreia Trianon <andreiatrianon@outlook.com>
2018-07-05 17:27:30 -0300

    solução

--- programadoras/assets/script.js
+++ programadoras/assets/script.js
@@ -1,10 +1,32 @@
+var dropMenu = document.getElementById("drop-menu");
 
+dropMenu.addEventListener("change", carregaProgramadoras);
 
+window.onload = carregaMenu();
 
-// function carregaMenu(){
+function carregaMenu(){
+    var nome = document.createElement("option");
+    nome.innerHTML = "selecione sede";
+    nome.value = "none";
+    dropMenu.appendChild(none);
 
-// };
+    for (sede in data) {
+        var itemMenu = document.createElement("option");
+        itemMenu.innerHTML = "sede";
+        itemMenu.value = "sede";
+        dropMenu.appendChild(itemMenu);
+    }
+};
 
 function carregaProgramadoras(){
-
+    var sede = dropMenu.value;
+    var listaProgramadoras = document.getElementById("exibe-programadoras");
+    listaProgramadoras.innerHTML = "";
+    for (turma in data[sede]) {
+        for (i in data[sede][turma]["students"]) {
+            var img = document.createElement("img");
+            img.src = data[sede][turma]["students"][i]["photo"];
+            listaProgramadoras.appendChild(img);
+        }
+    }
 };
